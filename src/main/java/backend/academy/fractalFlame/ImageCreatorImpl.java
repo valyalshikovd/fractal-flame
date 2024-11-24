f4cd78de785629845aca5b8c3f00fc7adea6afab
valyalshikovd <770vda@mail.ru>
2024-11-24 10:47:59 +0300

    refactor: refactor ColorMapper

--- src/main/java/backend/academy/fractalFlame/ImageCreatorImpl.java
+++ src/main/java/backend/academy/fractalFlame/ImageCreatorImpl.java
@@ -4,13 +4,19 @@ import java.awt.image.BufferedImage;
 
 public class ImageCreatorImpl implements ImageCreator {
 
+    private final ColorMapper colorMapper = new ColorMapperImpl();
+
     @Override
     public BufferedImage createImage(int sizeX, int sizeY, Point[][] point) {
 
 
         BufferedImage image = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_ARGB);
 
-        //todo
+        for (int x = 0; x < sizeX; x++){
+            for (int y = 0; y < sizeY; y++){
+                image.setRGB(x, y, colorMapper.mapColor(colorMapper.mapColor(point[x][y].position().getZ())));
+            }
+        }
 
         return image;
 
