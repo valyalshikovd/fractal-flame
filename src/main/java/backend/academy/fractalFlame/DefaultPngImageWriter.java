115a1ef7a85380582b53ec8b9a8a76497c655665
valyalshikovd <770vda@mail.ru>
2024-11-24 10:59:57 +0300

    refactor: refactor

--- src/main/java/backend/academy/fractalFlame/DefaultPngImageWriter.java
+++ src/main/java/backend/academy/fractalFlame/DefaultPngImageWriter.java
@@ -1,9 +1,9 @@
 package backend.academy.fractalFlame;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class DefaultPngImageWriter implements ImageWriter {
 
--- src/main/java/backend/academy/fractalFlame/ImageCreatorImpl.java
+++ src/main/java/backend/academy/fractalFlame/ImageCreatorImpl.java
@@ -12,8 +12,8 @@ public class ImageCreatorImpl implements ImageCreator {
 
         BufferedImage image = new BufferedImage(plot.sizeX(), plot.sizeY(), BufferedImage.TYPE_INT_ARGB);
 
-        for (int x = 0; x < plot.sizeX(); x++){
-            for (int y = 0; y < plot.sizeX(); y++){
+        for (int x = 0; x < plot.sizeX(); x++) {
+            for (int y = 0; y < plot.sizeX(); y++) {
                 image.setRGB(x, y, colorMapper.mapColor(colorMapper.mapColor(plot.arr()[x][y].position().getZ())));
             }
         }
