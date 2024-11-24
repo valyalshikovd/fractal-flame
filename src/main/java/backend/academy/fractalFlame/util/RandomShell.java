ef4ca7f96c01b56daca5f9d8100087219717f26c
valyalshikovd <770vda@mail.ru>
2024-11-24 11:24:01 +0300

    add: add utils

---
+++ src/main/java/backend/academy/fractalFlame/util/RandomShell.java
@@ -0,0 +1,5 @@
+package backend.academy.fractalFlame.util;
+
+public interface RandomShell {
+    int get(int size);
+}
---
+++ src/main/java/backend/academy/fractalFlame/util/juice/GuiceModule.java
@@ -0,0 +1,21 @@
+package backend.academy.fractalFlame.util.juice;
+
+import com.google.inject.AbstractModule;
+
+
+public class GuiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+//        bind(RandomShell.class).to(RandomShellImpl.class).in(Scopes.SINGLETON);
+//        bind(Renderer.class).to(RendererImpl.class).in(Scopes.SINGLETON);
+//        bind(UserInteractionService.class).to(UserInteractionServiceImpl.class).in(Scopes.SINGLETON);
+//        bind(ConsoleInputService.class).to(ConsoleInputServiceImpl.class).in(Scopes.SINGLETON);
+//        //bind(Generator.class).to(GrowingThreeMazeGenerator.class).in(Scopes.SINGLETON);
+//        bind(BufferedReaderShell.class).to(BufferedReaderShellImpl.class).in(Scopes.SINGLETON);
+//        bind(Generator.class).to(RecursiveBacktrackerGenerator.class).in(Scopes.SINGLETON);
+//        bind(Solver.class).to(DFSSolver.class).in(Scopes.SINGLETON);
+//        bind(CellFactory.class).to(ExtendedCellsFactory.class).in(Scopes.SINGLETON);
+
+    }
+}
